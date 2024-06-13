3dbe06d59963920017a31d8c0fe8a0148ec97051
AbhinavDevelops <abhinav.rajaram02@gmail.com>
2024-06-14 00:30:12 +0800

    first commit set up structure of progra

---
+++ build/CMakeFiles/my_backend.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_backend.dir/src/main.cpp.o"
+  "CMakeFiles/my_backend.dir/src/main.cpp.o.d"
+  "CMakeFiles/my_backend.dir/src/my_source.cpp.o"
+  "CMakeFiles/my_backend.dir/src/my_source.cpp.o.d"
+  "my_backend"
+  "my_backend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_backend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
